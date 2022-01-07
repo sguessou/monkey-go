df1e210c156a5c69706e3f18eb81014edb7ae198
Saad Guessous <guessous_saad@hotmail.com>
2022-01-07 21:46:42 +0200

    Extend lexer with additional keywords

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -17,7 +17,12 @@ let add = fn(x, y) {
 let result = add(five, ten);
 !-/*5;
 5 < 10 > 5;
-`
+
+if (5 < 10) {
+  return true;
+} else {
+   return false;
+}`
 
 	tests := []struct {
 		expectedType    token.TokenType
@@ -77,6 +82,24 @@ let result = add(five, ten);
 		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
 
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
 		{token.EOF, ""},
 	}
 
