1ba6ffb5826ad35948b43e9e3f50b8139670cda3
Saad Guessous <guessous_saad@hotmail.com>
2022-01-08 12:49:05 +0200

    Add parser logic

---
+++ parser/parser.go
@@ -0,0 +1,33 @@
+package parser
+
+import (
+	"github.com/sguessou/monkey-go/ast"
+	"github.com/sguessou/monkey-go/lexer"
+	"github.com/sguessou/monkey-go/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// Read two token, so curToken and peekToken are both set
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
